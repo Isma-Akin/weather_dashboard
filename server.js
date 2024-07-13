001476ed5fe4cd2e004ab8a458f305a12720897a
Isma-Akin <aakinsemoyin@hotmail.com>
2024-07-13 15:17:58 +0100

    refactor(server.js): utilize wttr.in API to fetch weather data | add error handling for API requests

--- server.js
+++ server.js
@@ -4,7 +4,6 @@ const path = require('path');
 const app = express();
 const port = 3000;
 
-const API_KEY = 'openweathermap_api_key';
 
 app.use(express.static(path.join(__dirname, 'public')));
 
@@ -15,22 +14,27 @@ app.get('/weather', async (req, res) => {
     }
 
     try {
-        const response = await axios.get(`https://api.openweathermap.org/data/2.5/weather`, {
-        params: {
-            q: city,
-            appid: API_KEY,
-            units: 'metric'
-        }
-        });
-        const data = response.data;
+        const encodedCity = encodeURIComponent(city);
+        const weatherResponse = await axios.get(`https://wttr.in/${encodedCity}?format=j1`);
+
+        const weatherData = weatherResponse.data;
         const weather = {
-            city: data.name,
-            temperature: data.main.temp,
-            description: data.weather[0].description,
-            icon: data.weather[0].icon
+            city: weatherData.nearest_area[0].areaName[0].value,
+            temperature: weatherData.current_condition[0].temp_C,
+            description: weatherData.current_condition[0].weatherDesc[0].value,
+            icon: weatherData.current_condition[0].weatherIconUrl[0].value
         };
         res.json(weather);
     } catch (error) {
+        if (error.response) {
+           console.error('Error response data:', error.response.data);
+           console.error('Error response status:', error.response.status);
+           console.error('Error response headers:', error.response.headers);
+        } else if (error.request) {
+           console.error('Error request:', error.request);
+        } else {
+           console.error('Error:', error.message);
+        }
         res.status(500).json({ error: 'Error fetching weather data'});
     }
 });
