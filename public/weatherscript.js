dcad5a02aa406b356c528e8f1c323d4f83cb5452
Isma-Akin <aakinsemoyin@hotmail.com>
2024-07-18 02:44:39 +0100

    refactor(weatherscript.js): modify alert message

--- public/weatherscript.js
+++ public/weatherscript.js
@@ -1,7 +1,7 @@
 async function getWeather() {
     const city = document.getElementById('city').value;
     if (!city) {
-        alert('Please enter a city name');
+        alert('Please enter a city name. \nE.g: London, New York');
         return;
     }
 
