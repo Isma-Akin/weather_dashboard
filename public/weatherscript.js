087a752a91eb2fa29cf4ef75a6debfed23a4b7d5
Isma-Akin <aakinsemoyin@hotmail.com>
2024-07-19 19:23:04 +0100

    refactor(weatherscript.js):duplicate getWeather function for clickable text column

--- public/weatherscript.js
+++ public/weatherscript.js
@@ -1,6 +1,6 @@
 async function getWeather() {
-    const city = document.getElementById('city').value;
-    if (!city) {
+       const city = document.getElementById('city').value;
+        if (!city) {
         alert('Please enter a city name. \nE.g: London, New York');
         return;
     }
@@ -16,6 +16,29 @@ async function getWeather() {
         <p>${data.description}</p>
         <img src="https://www.metaweather.com/static/img/weather/${data.icon}.svg" alt="Weather icon" alt="weather icon">
         
+        `;
+    } else {
+        alert(data.error);
+    }
+}
+
+async function getWeatherClick(city) {
+        if (!city) {
+        alert('Please enter a city name. \nE.g: London, New York');
+        return;
+    }
+
+    const response = await fetch(`/weather?city=${city}`);
+    const data = await response.json();
+
+    if (response.status === 200) {
+        const weatherDiv = document.getElementById('weather');
+        weatherDiv.innerHTML = `
+        <h2>${data.city}</h2>
+        <p>${data.temperature} °C</p>
+        <p>${data.description}</p>
+        <img src="https://www.metaweather.com/static/img/weather/${data.icon}.svg" alt="Weather icon" alt="weather icon">
+
         `;
     } else {
         alert(data.error);
