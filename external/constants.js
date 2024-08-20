7db1a2d70c84a212c365b22c8b377d6e0b6f4c4a
Isma-Akin <aakinsemoyin@hotmail.com>
2024-08-20 19:45:16 +0100

    refactor(constants.js): add constants.js

---
+++ external/constants.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+const axios = ('axios');
+
+module.exports = {
+    port,
+    app,
+    path,
+    express,
+    axios,
+}
