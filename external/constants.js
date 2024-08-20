121be5aa2c9eb2b47618fe2ee9076e59ca7fbd64
Isma-Akin <aakinsemoyin@hotmail.com>
2024-08-20 19:55:58 +0100

    fix(constants.js): require function for axios

--- external/constants.js
+++ external/constants.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const port = 3000;
-const axios = ('axios');
+const axios = require('axios');
 
 module.exports = {
     port,
