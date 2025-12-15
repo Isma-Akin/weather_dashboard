d445d34e590b1a688e9f901b444a4a92ec688bdd
Isma-Akin <aakinsemoyin@hotmail.com>
2025-12-15 23:55:58 +0000

    refactor(weatherimage): add comment

--- e2e/weatherimage.ts
+++ e2e/weatherimage.ts
@@ -1,6 +1,7 @@
 import { test, expect } from '@playwright/test';
 
 test('test', async ({ page }) => {
+  // Navigate to the application
   await page.goto('http://localhost:3000/');
   await expect(page.getByRole('link', { name: 'Cloud' })).toBeHidden();
   await page.getByPlaceholder('Enter city name').click();
