7ffd26aeb165e7be2168acc05f21f13de26ffb64
Isma-Akin <aakinsemoyin@hotmail.com>
2026-01-21 21:56:45 +0000

    refactor(cityname.spec.ts): modify test names; add assertion for image visibility test

--- e2e/cityname.spec.ts
+++ e2e/cityname.spec.ts
@@ -1,11 +1,16 @@
 import { test, expect } from '@playwright/test';
 
-test('test', async ({ page }) => {
+test('has text', async ({ page }) => {
   await page.goto('http://localhost:3000/');
   await page.getByRole('link', { name: 'London' }).click();
+  await expect(page.locator('#loader')).toBeVisible();
   await expect(page.locator('#weather')).toContainText('City of London Greater London');
+});
 
-  await page.getByPlaceholder('Enter city name').click();
-  await page.getByPlaceholder('Enter city name').fill('Brussels');
+test('has image div', async ({ page }) => {
+  await page.goto('http://localhost:3000/');
+  await page.locator('#city').click();
+  await page.locator('#city').fill('Brussels');
   await page.getByRole('button', { name: 'Get the weather' }).click();
+  await expect(page.locator('div').filter({ hasText: 'Brussels' }).nth(1)).toBeVisible();
 });
